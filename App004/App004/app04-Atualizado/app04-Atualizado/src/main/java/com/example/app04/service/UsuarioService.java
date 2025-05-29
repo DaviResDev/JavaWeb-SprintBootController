4d8b23c2a13d36adf715796c72658b1a9648028a
Davi Almeida <davilinkedinprofissional@gmail.com>
2025-05-29 10:57:53 -0300

    UsuarioService.java

--- App004/App004/app04-Atualizado/app04-Atualizado/src/main/java/com/example/app04/service/UsuarioService.java
+++ App004/App004/app04-Atualizado/app04-Atualizado/src/main/java/com/example/app04/service/UsuarioService.java
@@ -1,31 +1,21 @@
-package com.example.app04.service;
+package seu.pacote.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.example.app04.model.Usuario;
+import seu.pacote.model.Usuario;
+import seu.pacote.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-    private List<Usuario> usuarios = new ArrayList<>();
+
+    @Autowired
+    private UsuarioRepository repo;
 
     public void salvar(Usuario u) {
-        usuarios.add(u);
+        repo.save(u);
     }
 
-    public Usuario autenticar(String login, String senha) {
-        for (Usuario u : usuarios) {
-            if (u.getLogin().equals(login) && u.getSenha().equals(senha)) {
-                return u;
-            }
-        }
-        return null;
+    public Usuario autenticar(String email, String senha) {
+        return repo.findByEmailAndSenha(email, senha);
     }
-
-    public boolean validar(String login, String senha) {
-        return autenticar(login, senha) != null;
-    }
-
 }
